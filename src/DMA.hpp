1e25b82c37e345c43544e8b832d3905015646b3a
Rohan Kota <rohankota@Rohans-MacBook-Pro-6.local>
2024-07-18 22:23:27 -0500

    channel linking working

--- src/DMA.hpp
+++ src/DMA.hpp
@@ -55,24 +55,24 @@ class DMA {
 
         // Set up the serial port
         s.begin(SERIAL_8N1);
-        // LPUART BAUG Register: Page 2920
+        // LPUART BAUG Register (page 2920)
         // Oversampling Ratio: Set to 12 (bits 24-28)
         // Transmitter DMA Enable: Enable DMA requests (bit 23)
         // Receiver Full DMA Enable: Enable DMA requests (bit 21)
         // Baud Rate Modulo Divisor: Set to 1 (bits 0-12)
         (*LPUART).BAUD = (0b01011 << 24) | (0b1 << 23) | (0b1 << 21) | 0b1; 
-        // LPUART CTRL Register: Page 2926
+        // LPUART CTRL Register (page 2926)
         // Receiver Interrupt Enable: Hardware interrupt is requested when RDRF flag is 1 (bit 21)
         // Idle Line Interrupt Enable: Hardware interrupt is requested when IDLE flag is 1 (bit 20)
         (*LPUART).CTRL = (0b11 << 20);
-        // LPUART Modem IrDA Register: Page 2934
+        // LPUART Modem IrDA Register (page 2934)
         // Transmitter request-to-send polarity: Transmitter RTS is active high (bit 2)
         // Transmitter request-to-send enable (bit 1)
         (*LPUART).MODIR = (0b11 << 1);
-        // LPUART Watermark Register: Page 2939
+        // LPUART Watermark Register (page 2939)
         // Receive Watermark: Set to 1 (bits 16-17)
         (*LPUART).WATER = (0b01 << 16);
-        // LPUART Control Register: Page 2939
+        // LPUART Control Register (page 2939)
         // Transmitter Enable (bit 19)
         // Receiver Enable (bit 18)
         (*LPUART).CTRL |= (0b11 << 18);
@@ -122,6 +122,112 @@ class DMA {
         IMXRT_DMA_TCD[rx_ch].DADDR = dest;
         IMXRT_DMA_TCD[rx_ch].DOFF = 1;
         IMXRT_DMA_TCD[rx_ch].DLASTSGA = -2;
+        IMXRT_DMA_TCD[rx_ch].ATTR = 0;
+        IMXRT_DMA_TCD[rx_ch].CSR = 0;
+        DMA_SERQ = rx_ch; // Enable channel
+
+        // If the channel has an associated PIT timer, start the timer
+        if (tx_ch >= 0 && tx_ch <= 3) {
+            // Address of the appropriate PIT Timer Load Value Register
+            uint32_t* PIT_LDVAL = (uint32_t*)(0x40084100 + tx_ch * 0x10);
+            // Address of the appropriate PIT Timer Control Register
+            uint32_t* PIT_TCTRL = (uint32_t*)(0x40084108 + tx_ch * 0x10);
+
+            // Timer Load Value Register (page 3046)
+            // Set up timer for 2400 cycles (24 MHz clock / 2400 clock cycles = 1 kHz timer)
+            *PIT_LDVAL = 2400 - 1;
+
+            // Timer Control Register (page 3048)
+            // Timer Interrupt Enable: Interrupt is requested whenever TIF is set (bit 1)
+            // Timer Enable (bit 0)
+            *PIT_TCTRL = 0b11;
+        }
+    }
+
+    /**
+     * Sets up a UART TX Link
+     *
+     * @param serial_ch The number of the HarwareSerial channel to be used.
+     * @param src A pointer to the address of the encoder value(s) to be transmitted
+     * @param dest A pointer to where the incoming encoder value(s) should be stored
+     */
+    void addLinkChannel(const int serial_ch, const void* src, volatile void* dest) {
+        int tx_ch = next_dma_ch_; // The DMA channel number that will be used for transmitting data over UART
+        int rx_ch = next_dma_ch_ + 16; // The DMA channel number that will be used for receiving data over UART
+        next_dma_ch_++; // Increment the next available DMA channel
+
+        HardwareSerialIMXRT s = serial_channels[serial_ch - 1]; // Obtain the appropriate HardwareSerial port
+        IMXRT_LPUART_t* LPUART = (IMXRT_LPUART_t*)(0x40184000 + (lpuart_channels[serial_ch-1] - 1) * 0x4000); // Pointer to the LPUART registers corresponding to the desired HardwareSerial port
+
+        /**** Set Up UART Channel ****/
+
+        // Set up the serial port
+        s.begin(SERIAL_8N1);
+        // LPUART BAUG Register (page 2920)
+        // Oversampling Ratio: Set to 12 (bits 24-28)
+        // Transmitter DMA Enable: Enable DMA requests (bit 23)
+        // Receiver Full DMA Enable: Enable DMA requests (bit 21)
+        // Baud Rate Modulo Divisor: Set to 1 (bits 0-12)
+        (*LPUART).BAUD = (0b01011 << 24) | (0b1 << 23) | (0b1 << 21) | 0b1; 
+        // LPUART CTRL Register (page 2926)
+        // Receiver Interrupt Enable: Hardware interrupt is requested when RDRF flag is 1 (bit 21)
+        // Idle Line Interrupt Enable: Hardware interrupt is requested when IDLE flag is 1 (bit 20)
+        (*LPUART).CTRL = (0b11 << 20);
+        // LPUART Modem IrDA Register (page 2934)
+        // Transmitter request-to-send polarity: Transmitter RTS is active high (bit 2)
+        // Transmitter request-to-send enable (bit 1)
+        (*LPUART).MODIR = (0b11 << 1);
+        // LPUART Watermark Register (page 2939)
+        // Receive Watermark: Set to 3 (bits 16-17)
+        (*LPUART).WATER = (0b11 << 16);
+        // LPUART Control Register (page 2939)
+        // Transmitter Enable (bit 19)
+        // Receiver Enable (bit 18)
+        (*LPUART).CTRL |= (0b11 << 18);
+        
+        // Address for DMA MUX Channel Config Register
+        uint32_t* TX_CHANNEL_CONFIG = (uint32_t*)(0x400EC000 + tx_ch * 0x04);
+        uint32_t* RX_CHANNEL_CONFIG = (uint32_t*)(0x400EC000 + rx_ch * 0x04);
+        
+        /**** Init DMA TX channel that sends the encoder read command ****/
+
+        // DMA MUX Channel Configuration Register (page 85)
+        // DMA Mux Channel Enable (bit 31)
+        // DMA Channel Trigger Enable: Set to periodic trigger mode (bit 30)
+        // DMA Channel Source: Set to appropriate DMA_MUX_LPUART_TX source (bits 0-6)
+        *TX_CHANNEL_CONFIG = ((0b1 << 31) | (0b1 << 30) | TX_DMAMUX_SOURCES[lpuart_channels[serial_ch-1]-1]);
+
+        // Configure the TCD for the DMA channel
+        IMXRT_DMA_TCD[tx_ch].CITER = 1;
+        IMXRT_DMA_TCD[tx_ch].BITER = 1;
+        IMXRT_DMA_TCD[tx_ch].NBYTES = 6;
+        IMXRT_DMA_TCD[tx_ch].SADDR = src;
+        IMXRT_DMA_TCD[tx_ch].SOFF = 1;
+        IMXRT_DMA_TCD[tx_ch].SLAST = -6;
+        IMXRT_DMA_TCD[tx_ch].DADDR = &((*LPUART).DATA);
+        IMXRT_DMA_TCD[tx_ch].DOFF = 0;
+        IMXRT_DMA_TCD[tx_ch].DLASTSGA = 0;
+        IMXRT_DMA_TCD[tx_ch].ATTR = 0;
+        IMXRT_DMA_TCD[tx_ch].CSR = 0;
+        DMA_SERQ = tx_ch; // Enable channel
+
+        /**** Init DMA RX channel that places the encoder reading in memory ****/
+
+        // DMA MUX Channel Configuration Register (page 85)
+        // DMA Mux Channel Enable (bit 31)
+        // DMA Channel Source: Set to appropriate DMA_MUX_LPUART_RX source (bits 0-6)
+        *RX_CHANNEL_CONFIG = ((0b1 << 31) | RX_DMAMUX_SOURCES[lpuart_channels[serial_ch-1]-1]);
+
+        // Configure the TCD for the DMA channel
+        IMXRT_DMA_TCD[rx_ch].CITER = 1;
+        IMXRT_DMA_TCD[rx_ch].BITER = 1;
+        IMXRT_DMA_TCD[rx_ch].NBYTES = 6;
+        IMXRT_DMA_TCD[rx_ch].SADDR = &((*LPUART).DATA);
+        IMXRT_DMA_TCD[rx_ch].SOFF = 0;
+        IMXRT_DMA_TCD[rx_ch].SLAST = 0;
+        IMXRT_DMA_TCD[rx_ch].DADDR = dest;
+        IMXRT_DMA_TCD[rx_ch].DOFF = 1;
+        IMXRT_DMA_TCD[rx_ch].DLASTSGA = -6;
         IMXRT_DMA_TCD[rx_ch].ATTR = 0;
         IMXRT_DMA_TCD[rx_ch].CSR = 0;
         DMA_SERQ = rx_ch; // Enable channel
--- src/encoders.cpp
+++ src/encoders.cpp
@@ -5,25 +5,52 @@
 constexpr uint8_t encoder_command = 0x54;
 volatile uint16_t hcom, vcom;
 
+// Init LINK buffers
+uint16_t encoder_vals[5];
+uint16_t rec_link[3];
+
 const float maxEncoderCount = pow(2, 14);
 
 DMA dma;
 
+int i = 0;
+
 void setup() {
-  
-  dma.addUARTEncoder(1, &encoder_command, &hcom);
-  dma.addUARTEncoder(3, &encoder_command, &vcom);
-  
+  encoder_vals[0] = 0x15;
+
+  dma.addUARTEncoder(4, &encoder_command, encoder_vals+1); // HPOS
+  dma.addUARTEncoder(8, &encoder_command, encoder_vals+2); // VPOS
+  dma.addUARTEncoder(1, &encoder_command, encoder_vals+3); // HCOM
+  dma.addUARTEncoder(3, &encoder_command, encoder_vals+4); // VCOM
+
+  dma.addLinkChannel(6, encoder_vals, rec_link);
+
+  IMXRT_LPUART_t* LPUART = (IMXRT_LPUART_t*)(0x40184000 + (5) * 0x4000); // Pointer to the LPUART registers corresponding to the desired HardwareSerial port
+
+  uint32_t temp;
+  (*LPUART).CTRL &= ~(0b11 << 18);
+  while ((*LPUART).CTRL & (0b11<<18)) {;}
+  // while (rec_link[0] != 0x0015) {
+  //   temp = (*LPUART).DATA;
+  // }
+  (*LPUART).CTRL |= (0b11<<18);
+
 }
 
 void loop() {
-
-  float hcom_ang = 2.0 * PI * static_cast<float>(hcom & 0x3FFF) / maxEncoderCount ;
-  float vcom_ang = 2.0 * PI * static_cast<float>(vcom & 0x3FFF) / maxEncoderCount ;
-
-  Serial.print("hcom: ");
-  Serial.print(hcom_ang);
-  Serial.print(", vcom: ");
-  Serial.println(vcom_ang);
   
+  if ((i++ % 1000) == 0) {
+    // float hcom_ang = 2.0 * PI * static_cast<float>(snd_link[0] & 0x3FFF) / maxEncoderCount ;
+    // float vcom_ang = 2.0 * PI * static_cast<float>(snd_link[1] & 0x3FFF) / maxEncoderCount ;
+    float hcom_ang = 2.0 * PI * static_cast<float>(rec_link[1] & 0x3FFF) / maxEncoderCount ;
+    float vcom_ang = 2.0 * PI * static_cast<float>(rec_link[2] & 0x3FFF) / maxEncoderCount ;
+
+    Serial.print("parity: ");
+    Serial.print(rec_link[0]);
+    Serial.print(", hcom: ");
+    Serial.print(hcom_ang);
+    Serial.print(", vcom: ");
+    Serial.println(vcom_ang);
+
+  }
 }
